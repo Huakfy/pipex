cd670e955c9a0b01c564842a673b6b20c1e5416d
Marc <marc@DESKTOP-G59U5LS.localdomain>
2023-02-09 16:55:46 +0100

    norm

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mjourno <mjourno@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 12:01:35 by mjourno           #+#    #+#             */
-/*   Updated: 2023/02/09 16:30:16 by mjourno          ###   ########.fr       */
+/*   Updated: 2023/02/09 16:55:22 by mjourno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,100 +109,90 @@ char	*find_path(char *command, char **paths, t_data *data)
 	return (NULL);
 }
 
+void	child_process1(t_data *data, char **argv, char **envp)
+{
+	char *path;
+
+	data->pid1 = fork();
+	if (data->pid1 < 0)
+		free_all(data, 1);
+	if (!data->pid1)
+	{
+		if (dup2(data->input, STDIN_FILENO) < 0)
+			free_all(data, 1);
+		if (dup2(data->pipe[1], STDOUT_FILENO) < 0)
+			free_all(data, 1);
+		close(data->pipe[0]);
+		close(data->input);
+		data->arg = ft_split(argv[2], ' ');
+		if (!data->arg)
+			free_all(data, 1);
+		path = find_path(data->arg[0], data->paths, data);
+		if (execve(path, data->arg, envp) == -1)
+		{
+			free(path);
+			free_all(data, 1);
+		}
+		free(path);
+	}
+}
+
+void	child_process2(t_data *data, char **argv, char **envp)
+{
+	data->pid2 = fork();
+	if (data->pid2 < 0)
+		free_all(data, 1);
+	if (!data->pid2)
+	{
+		if (dup2(data->output, STDOUT_FILENO) < 0)
+			free_all(data, 1);
+		if (dup2(data->pipe[0], STDIN_FILENO) < 0)
+			free_all(data, 1);
+		close(data->pipe[1]);
+		close(data->output);
+		data->arg = ft_split(argv[3], ' ');
+		if (!data->arg)
+			free_all(data, 1);
+		char *path;
+		path = find_path(data->arg[0], data->paths, data);
+		if (execve(path, data->arg, envp) == -1)
+		{
+			free(path);
+			free_all(data, 1);
+		}
+		free(path);
+	}
+}
+
+void	verify_files(t_data *data, int argc, char **argv)
+{
+	data->input = open(argv[1], O_RDONLY);
+	if (data->input < 0)
+		exit (ft_printf("%s: %s\n",strerror(errno), argv[1]));
+	data->output = open(argv[argc - 1], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	if (data->output < 0)
+	{
+		free_all(data, 0);
+		exit (ft_printf("%s: %s\n",strerror(errno), argv[argc - 1]));
+	}
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	t_data	data;
 
 	init_args(&data);
-	//Pour partie obligatoire, seulement 4 arguments
 	if (argc != 5)
 		exit (ft_printf("Error\nMust have 4 arguments\n"));
-
-	//Verification de l'existance et des droits de l'input et output
-	data.input = open(argv[1], O_RDONLY);
-	if (data.input < 0)
-		exit (ft_printf("%s: %s\n",strerror(errno), argv[1]));
-	data.output = open(argv[argc - 1], O_CREAT | O_RDWR | O_TRUNC, 0644);
-	if (data.output < 0)
-	{
-		free_all(&data, 0);
-		exit (ft_printf("%s: %s\n",strerror(errno), argv[argc - 1]));
-	}
-
-	//Creation du pipe
 	if (pipe(data.pipe) < 0)
 		free_all(&data, 1);
-
-	//Recuperer les differents paths
 	get_paths(envp, &data);
-
-	data.pid1 = fork();
-	if (data.pid1 < 0)
-		free_all(&data, 1);
-	if (!data.pid1)
-	{
-		//child1 process
-		if (dup2(data.input, STDIN_FILENO) < 0)
-			free_all(&data, 1);
-		if (dup2(data.pipe[1], STDOUT_FILENO) < 0)
-			free_all(&data, 1);
-		close(data.pipe[0]);
-		close(data.input);
-
-		//cmd + arg + execve
-		//Split la commande de ces arguments
-		data.arg = ft_split(argv[2], ' ');
-		if (!data.arg)
-			free_all(&data, 1);
-		//Trouver le path de la commande
-		char *path;
-		path = find_path(data.arg[0], data.paths, &data);
-
-		if (execve(path, data.arg, envp) == -1)
-		{
-			free(path);
-			free_all(&data, 1);
-		}
-		free(path);
-	}
-
+	child_process1(&data, argv, envp);
 	if (data.error == 1)
 		free_all(&data, 1);
-
-	data.pid2 = fork();
-	if (data.pid2 < 0)
-		free_all(&data, 1);
-	if (!data.pid2)
-	{
-		//child2 process
-		if (dup2(data.output, STDOUT_FILENO) < 0)
-			free_all(&data, 1);
-		if (dup2(data.pipe[0], STDIN_FILENO) < 0)
-			free_all(&data, 1);
-		close(data.pipe[1]);
-		close(data.output);
-
-		//cmd + arg + execve
-		//Split la commande de ces arguments
-		data.arg = ft_split(argv[3], ' ');
-		if (!data.arg)
-			free_all(&data, 1);
-		//Trouver le path de la commande
-
-		char *path;
-		path = find_path(data.arg[0], data.paths, &data);
-
-		if (execve(path, data.arg, envp) == -1)
-		{
-			free(path);
-			free_all(&data, 1);
-		}
-		free(path);
-	}
-
+	child_process2(&data, argv, envp);
 	if (data.error == 1)
 		free_all(&data, 1);
-
 	if (!data.pid1)
 		waitpid(data.pid1, NULL, 0);
 	if (!data.pid2)
